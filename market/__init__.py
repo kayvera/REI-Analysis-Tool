22e38b0aad6069914da737fb2d1307d52dee8028
kayvera <mikaylarvr@gmail.com>
2021-08-09 22:27:22 -0700

    added form validation

--- market/__init__.py
+++ market/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
-
+app.config['SECRET_KEY'] = 'e3310b9818fb954339c8cc3f'
 db = SQLAlchemy(app)
 
 from market import routes
